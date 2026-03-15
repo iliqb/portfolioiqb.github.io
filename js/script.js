36e7e1244a00bb9ab01520ff27d051a988df69ae
iliqb <134982157+iliqb@users.noreply.github.com>
2026-03-15 12:54:50 -0600

    impruve style

--- js/script.js
+++ js/script.js
@@ -27,3 +27,23 @@ prevButton.addEventListener("click", () => {
   }
   updateCarousel();
 });
+
+// dark-mode
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("darkModeToggle");
+
+  if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+    toggle.checked = true;
+  }
+
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("darkMode", "disabled");
+    }
+  });
+});
